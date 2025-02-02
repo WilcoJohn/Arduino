b8c2c72a25c296494b4f61039ef6358c03008950
WilcoJohn <sieverswilcojohn@gmail.com>
2025-02-02 15:28:03 +0200

    Update HT_LCD_display.ino

--- HT_LCD_display/HT_LCD_display.ino
+++ HT_LCD_display/HT_LCD_display.ino
@@ -1,18 +1,27 @@
 #include "htData.h"
+#include "lcdDisplay.h"
 
 
 
-HTSensor htData{A0};
+HTSensor htData{A0};          // create temperature and humidity sensor object
+lcdDisplay screen{};          // create LCD screen object
 
 void setup() 
 {
-  Serial.begin(9600);
+  
+  Serial.begin(9600);         // serial setup
+  screen.init();              // LCD screen display set up
 }
 
 void loop() 
 {
-  htData.read();
-  htData.print();
+  htData.readSensor();        // update data
+  htData.serialPrint();       // print data to serial
+  
+  
+  // print data to LCD screen
+  screen.updateValues( htData.getH(), htData.getT());
 
-  delay(htData.getDelay());
+ 
+  delay(htData.getDelay());   // delay data until sensor can update again
 }
